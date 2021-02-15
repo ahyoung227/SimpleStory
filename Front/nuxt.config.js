cb8541ee855e09613dc99d4cbc4d211b75c3b976
ahyoungkim <ahyoung227@gmail.com>
2021-02-15 00:40:00 -0500

    Update for aws clone Update start script return start script

--- Front/nuxt.config.js
+++ Front/nuxt.config.js
@@ -44,5 +44,8 @@ module.exports = {
         browserBaseURL: 'http://localhost:3085',
         baseURL: 'http://localhost:3085',
         https: false,
+    },
+    server: {
+        port: process.env.PORT || 3085
     }
 };
