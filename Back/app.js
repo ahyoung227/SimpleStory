350d190ad5e4820e978829405254c1141e2d504f
ahyoung Kim <ahyoungkim@ahyoungs-iMac.home>
2021-03-08 23:41:15 -0500

    reset port number

--- Back/app.js
+++ Back/app.js
@@ -4,11 +4,7 @@ const passport = require('passport');
 const session = require('express-session');
 const cookie = require('cookie-parser');
 const morgan = require('morgan');
-const hpp = require('hpp');
-const helmet = require('helmet');
-const dotenv = require('dotenv');
 
-const prod = process.env.NODE_ENV === 'production';
 const db = require('./models');
 const passportConfig = require('./passport');
 const userRouter = require('./routes/user');
@@ -17,38 +13,25 @@ const postsRouter = require('./routes/posts');
 const hashtagRouter = require('./routes/hashtag');
 const app = express();
 
-dotenv.config();
 db.sequelize.sync();
 passportConfig();
 
-if(prod) {
-    app.use(helmet());
-    app.use(hpp());
-    app.use(morgan('combined'));
-    app.use(cors({
-        origin: 'http://simplestory.ga',
-        credentials: true,
-    }));
-} else {
-    app.use(morgan('dev'))
-    app.use(cors({
-        origin: 'http://localhost:3085',
-        credentials: true,
-    }));
-}
-
+app.use(morgan('dev'))
+app.use(cors({
+    origin: 'http://localhost:3080',
+    credentials: true,
+}));
 app.use('/', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookie(process.env.COOKIE_SECRET));
+app.use(cookie('cookiesecret'));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
+    secret: "cookiesecret",
     cookie: {
         httpOnly: true,
         secure: false,
-        domain: prod && '.simplestory.ga'
     },
 }));
 app.use(passport.initialize());
@@ -63,6 +46,6 @@ app.use('/post', postRouter);
 app.use('/posts', postsRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(prod ? process.env.PORT : 3085, () => {
-    console.log(`backend ${prod ? process.env.PORT : 3085} is ready`)
+app.listen(3085, () => {
+    console.log(`backend ${3085} is ready`)
 })
