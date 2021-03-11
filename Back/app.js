6cec78dad86dede97af98ba49ab1a345406adc8e
ahyoungkim <ahyoung227@gmail.com>
2021-03-10 21:49:07 -0500

    Fix bugs

--- Back/app.js
+++ Back/app.js
@@ -4,6 +4,9 @@ const passport = require('passport');
 const session = require('express-session');
 const cookie = require('cookie-parser');
 const morgan = require('morgan');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
 
 const prod = process.env.NODE_ENV === 'production';
 const db = require('./models');
@@ -14,6 +17,7 @@ const postsRouter = require('./routes/posts');
 const hashtagRouter = require('./routes/hashtag');
 const app = express();
 
+dotenv.config();
 db.sequelize.sync();
 passportConfig();
 
@@ -38,14 +42,15 @@ app.use(morgan('dev'))
 app.use('/', express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookie('cookiesecret'));
+app.use(cookie(process.env.COOKIE_SECRET));
 app.use(session({
     resave: false,
     saveUninitialized: false,
-    secret: "cookiesecret",
+    secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true,
         secure: false,
+        domain: prod && '.simplestory.gam',
     },
 }));
 app.use(passport.initialize());
@@ -61,5 +66,5 @@ app.use('/posts', postsRouter);
 app.use('/hashtag', hashtagRouter);
 
 app.listen(3085, () => {
-    console.log(`backend ${3085} is ready`)
+    console.log(`backend ${prod ? process.env.PORT : 3085} is ready`)
 })
